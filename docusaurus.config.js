3e0edd5b5a69cf84eebde72d62119e548e3a2897
mcyydscc <90653809+mcyydscc@users.noreply.github.com>
2022-11-19 01:51:17 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/mcyydscc/Forensics-wiki/',
+            'https://github.com/mcyydscc/Forensics-wiki/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/mcyydscc/Forensics-wiki/',
+            'https://github.com/mcyydscc/Forensics-wiki/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
