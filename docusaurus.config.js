53277f01951d37e41880b28082f9803c35b9eb1d
mcyydscc <90653809+mcyydscc@users.noreply.github.com>
2022-11-19 01:38:23 +0800

    first upload

---
+++ docusaurus.config.js
@@ -0,0 +1,144 @@
+// @ts-nocheck
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Forensics-Wiki',
+  tagline: '电子数据取证Wiki',
+  url: 'https://www.forensics-wiki.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Forensics-wiki',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'introduce',
+            position: 'left',
+            label: '开始',
+          },
+          {
+            to: '/blog',
+            position: 'left',
+            label: '零碎知识',
+          },
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: '文档',
+            items: [
+              {
+                label: '开始',
+                to: '/docs/introduce',
+              },
+              {
+                label: '取证基础',
+                to: '/docs/category/取证基础',
+              },
+              {
+                label: '加密与解密',
+                to: '/docs/category/加密与解密',
+              },
+              {
+                label: '内存取证volatility',
+                to: '/docs/category/内存取证volatility',
+              },
+            ],
+          },
+          {
+            title: '相关链接',
+            items: [
+              {
+                label: '电子取证靶场',
+                href: 'https://forensics.didctf.com',
+              },
+              {
+                label: '数据安全协创社团',
+                href: 'https://www.didctf.com',
+              },
+            ],
+          },
+          {
+            title: '更多',
+            items: [
+              {
+                label: 'My Blog',
+                href: 'https://blog.didctf.com',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Forensics-Wiki, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
