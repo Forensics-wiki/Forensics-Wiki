825a0c4a47bc9c546802da6017bb19305994628f
ZhangZ <1762326648@qq.com>
2023-07-30 19:01:10 +0800

    51la

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,18 +3,28 @@ import theme from "./theme.js";
 import { hopeTheme } from "vuepress-theme-hope";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 
-const statisticsScript = `
-<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>
-<script>LA.init({id:"K2xUaYGSFQf9s7gd",ck:"K2xUaYGSFQf9s7gd"});</script>
-`;
-
 export default defineUserConfig({
   base: "/",
   lang: "zh-CN",
   title: "Forensics-Wiki",
   description: "电子数据取证综合文库",
   theme,
-  
+  head: [
+    // 添加统计代码的 <script> 标签
+    [
+      "script",
+      {
+        charset: "UTF-8",
+        id: "LA_COLLECT",
+        src: "//sdk.51.la/js-sdk-pro.min.js",
+      },
+    ],
+    [
+      "script",
+      {},
+      `LA.init({id:"K2xUaYGSFQf9s7gd",ck:"K2xUaYGSFQf9s7gd"})`,
+    ],
+  ],
   plugins: [
     searchProPlugin({
       indexContent: true,
