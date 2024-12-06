e4261533bd1ab32b0ea7062f26ccc3009bdf5782
ZhangZ <1762326648@qq.com>
2024-12-06 15:20:41 +0800

    bird

--- docs/js/baidu.js
+++
@@ -1,6 +0,0 @@
-var _hmt = _hmt || []; (function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?ae653a122862bf27cab2753ef24126ae";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
---
+++ docs/js/bird.js
@@ -0,0 +1,1 @@
+document.write("<script>!(function () {\"use strict\";var w = window, d = document, u = \"//api.tongjiniao.com/c?_=723557576097275904\", s =document.createElement(\"script\"), r = document.getElementsByTagName(\"script\")[0];s.type = \"text/javascript\";s.setAttribute(\"charset\", \"UTF-8\");s.async = !0;s.src = u;r.parentNode.insertBefore(s, r)})();</script>")
