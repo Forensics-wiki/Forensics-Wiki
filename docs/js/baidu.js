1cca3f97076a64acaa7435b2b3794c49c1546dff
ZhangZ <1762326648@qq.com>
2024-12-06 15:11:08 +0800

    Modify 51la to Baidu Statistics

---
+++ docs/js/baidu.js
@@ -0,0 +1,6 @@
+var _hmt = _hmt || []; (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?ae653a122862bf27cab2753ef24126ae";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
