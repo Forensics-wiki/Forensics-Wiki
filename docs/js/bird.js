8e82373fc6415bff2aac31c987a5da3ed7ff39b9
ZhangZ <90653809+mcyydscc@users.noreply.github.com>
2025-03-01 02:08:18 +0800

    change js

--- docs/js/bird.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    var script = document.createElement('script');
-    script.src = "https://statistics.didctf.com/script.js";
-    script.setAttribute('data-website-id', 'e8774b1e-7d08-4f30-b6e2-af5040da535d');
-    script.defer = true;
-    document.head.appendChild(script);
-})();
