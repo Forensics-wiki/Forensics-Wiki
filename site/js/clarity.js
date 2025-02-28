5c854b8c904e25e81fc031a07b31ba9f03700c94
ZhangZ <90653809+mcyydscc@users.noreply.github.com>
2025-03-01 02:02:38 +0800

    change js

--- site/js/clarity.js
+++
@@ -1,8 +0,0 @@
-(function(c, l, a, r, i, t, y) {
-    c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) };
-    t = l.createElement(r);
-    t.async = 1;
-    t.src = "https://www.clarity.ms/tag/" + i;
-    y = l.getElementsByTagName(r)[0];
-    y.parentNode.insertBefore(t, y);
-})(window, document, "clarity", "script", "qh08xw12b1");
